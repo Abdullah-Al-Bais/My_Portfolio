bcb841860a703ef0ff5b16f79190fb8a67d4e1fb
Abdullah-Al-Bais <aabais62@gmail.com>
2025-02-10 02:03:21 +0600

    1st work of my portfolio

---
+++ script.js
@@ -0,0 +1,33 @@
+
+//Changing Button Style When Clicked
+const sectBtn = document.querySelectorAll('.control');
+function pageTransitions(){
+    for(let i = 0; i < sectBtn.length; i++){
+        sectBtn[i].addEventListener('click', function(){
+            let currentBtn = document.querySelectorAll('.active-btn');
+            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
+            this.className += ' active-btn';
+        })
+    }
+}
+pageTransitions();
+
+//toggle switch for darkmode
+const toggleMode = document.querySelector("#toggleMode");
+const body = document.querySelector("body");
+
+toggleMode.addEventListener("change", ()=> {
+    if (toggleMode.checked) {
+        body.classList.add("darkmode"); // Enable dark mode
+    } else {
+        body.classList.remove("darkmode"); // Disable dark mode
+    }
+});
+
+//set width of hr according to the percentage of the skill
+
+const percentage = document.querySelectorAll(".percentage");
+const hr = document.querySelectorAll(".skill-hr");
+for(let i = 0; i < percentage.length; i++){
+    hr[i].style.width = percentage[i].innerText;
+}
